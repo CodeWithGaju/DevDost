80af52e4669409464ffde7c17acf29d3ab396c9c
Gajanand Khatriya <164136299+CodeWithGaju@users.noreply.github.com>
2024-10-02 20:17:19 +0530

    exploring get,post,delete in app.js

--- src/app.js
+++ src/app.js
@@ -2,16 +2,37 @@ const express = require('express');
 
 const app = express();
 
-app.use("/home",(req,res)=>{
-    res.send("Welcome You are at Home Page");
+app.delete("/users",(req,res)=>{
+    //database data is deleted
+    res.send("Data is Deleted from Db")
+ })
+app.post("/users",(req,res)=>{
+   //database data is updated;
+   res.send("Data is posted or updated in Db")
 })
-app.use("/hello",(req,res)=>{
+
+app.get("/users",(req,res)=>{
+    res.send({
+        name:"Gajanand",
+        lastName: "Khatriya"
+    })
+})
+
+app.use("/hello/2",(req,res)=>{
     res.send("Hello Hello")
 })
+
+app.use("/hello",(req,res)=>{
+    res.send("Hello ")
+})
+
 app.use("/test",(req,res)=>{
      res.send("You are on Test page");
 })
 
+app.use("/",(req,res)=>{
+    res.send("Welcome You are at Home Page");
+})
 app.listen(2001,()=>{
     console.log("Server is Successfuly listening on port 2001")
 })
