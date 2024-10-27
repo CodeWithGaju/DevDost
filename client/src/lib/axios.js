801bf42a5a72ebcf929792f39635aad393623b9a
Gajanand Khatriya <164136299+CodeWithGaju@users.noreply.github.com>
2024-10-27 14:21:37 +0530

    Project Completed

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -2,6 +2,6 @@ import axios  from "axios";
 //TODO update the base url here so that it works in deployment as well
 const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:2001/api" : "/api";
 export const axiosInstance = axios.create({
-    baseURL: BASE,
+    baseURL: BASE_URL,
     withCredentials: true,
 })
