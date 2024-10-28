20e35eda9b701944057cdd0b5168a6e54c881c73
Gajanand Khatriya <164136299+CodeWithGaju@users.noreply.github.com>
2024-10-28 09:22:30 +0530

    Project Completed

--- api/server.js
+++ api/server.js
@@ -35,7 +35,7 @@ app.use("/api/user",userRouter);
 app.use("/api/message",messageRouter);
 
 if(process.env.NODE_ENV === 'production'){
-  app.use(express.static(path.join(__dirname,"/client/dist/index.html")))
+  app.use(express.static(path.join(__dirname,"/client/dist")));
   console.log(__dirname);
   app.get("*",(req,res)=>{
     res.sendFile(path.resolve(__dirname,"client","dist","index.html"))
